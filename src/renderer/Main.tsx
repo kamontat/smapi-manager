669140421e7f2b616b93bf6b645dc4ed6fc46ab4
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-01-11 01:06:18 +0700

    chore: move fixed page as demo component

--- src/renderer/Main.tsx
+++ src/renderer/Main.tsx
@@ -1,32 +1,33 @@
 import React, { useState } from "react";
-import "twin.macro";
+import tw from "twin.macro";
+import Content from "./components/Content";
+import Navbar from "./components/Navbar";
+
+const Container = tw.div`
+  flex h-full min-h-screen
+`;
 
 const Main = (): JSX.Element => {
-  const mapper = {
-    first: <span>first page</span>,
-    second: <span>second page</span>,
-  };
+  const navbar = [
+    {
+      key: "first",
+      name: "First",
+      element: <span>First page</span>,
+    },
+    {
+      key: "second",
+      name: "Second",
+      element: <span>Second page</span>,
+    },
+  ];
 
-  type Mapper = typeof mapper;
-
-  const [name, setName] = useState("first" as keyof Mapper);
-
-  const switchPage = (name: keyof Mapper) => {
-    return () => setName(name);
-  };
+  const [content, setContent] = useState(navbar[0]);
 
   return (
-    <div tw="flex h-full min-h-screen">
-      <nav tw="w-1/3">
-        <div>
-          <button onClick={switchPage("first")}>first</button>
-        </div>
-        <div>
-          <button onClick={switchPage("second")}>second</button>
-        </div>
-      </nav>
-      <div tw="w-2/3">{mapper[name]}</div>
-    </div>
+    <Container>
+      <Navbar data={navbar} updater={setContent} />
+      <Content>{content.element}</Content>
+    </Container>
   );
 };
 
---
+++ src/renderer/components/Content.tsx
@@ -0,0 +1,7 @@
+import tw from "twin.macro";
+
+const Content = tw.div`
+  w-full md:w-2/3 px-4 py-3
+`;
+
+export default Content;
