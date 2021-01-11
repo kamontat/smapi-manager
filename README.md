04b2934f955983ff13a68801088895241adb43a0
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-01-11 21:07:29 +0700

    chore(config): add webpack bundle analyse size

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # SMAPI Manager application
 
 Built with Electron and Typescript.
+
+### Analyse
+
+I added webpack bundle analyse data in 3 modules
+
+1. main - `.webpack/main/reports.main.html`
+2. preload - `.webpack/renderer/main_window/reports.renderer.html`
+3. renderer - `.webpack/renderer/reports.renderer.html`
--- webpack/webpack.plugins.js
+++ webpack/webpack.plugins.js
@@ -1,6 +1,13 @@
+const path = require("path") // eslint-disable-line @typescript-eslint/no-var-requires
+
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin // eslint-disable-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin'); // eslint-disable-line @typescript-eslint/no-var-requires
 
-
-module.exports = [
-  new ForkTsCheckerWebpackPlugin()
-];
+module.exports = (name) => []
+  .concat(process.env.CI !== "true" ? new BundleAnalyzerPlugin({
+    analyzerMode: "static",
+    reportFilename: path.join(`reports.${name}.html`),
+    openAnalyzer: false
+  }) : [])
+  .concat(name === "renderer" ? new ForkTsCheckerWebpackPlugin(
+  ) : []);
