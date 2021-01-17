a79376640765b3dee79bd6a78b00f92a43d6466b
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-01-18 00:45:52 +0700

    refac(common): remove models in common

---
+++ src/common/application/index.ts
@@ -0,0 +1,4 @@
+export { defaults as defaultAppInfo } from "./app-info";
+export type { AppInfo } from "./app-info";
+export { defaults as defaultElectronInfo } from "./electron-info";
+export type { ElectronInfo } from "./electron-info";
