c2c345549ceef2e2fa40e40ac8048453d583a323
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-01-18 00:54:38 +0700

    perf(comp): add message badge for new listing component

--- src/renderer/components/Badge.tsx
+++ src/renderer/components/Badge.tsx
@@ -1,11 +1,17 @@
 import tw from "twin.macro";
 
 const Badge = tw.span`
-  px-2 inline-flex text-xs leading-5 rounded-full cursor-default
+  text-xs leading-5 rounded-full cursor-default
+  px-2 md:px-4 py-1
 `;
 
-const InfoBadge = tw(Badge)`bg-green-100 text-green-800`;
-const WarnBadge = tw(Badge)`bg-red-100 text-red-800`;
+const BadgeContainer = tw.div`
+  flex space-x-3 mt-3 mb-2
+`;
 
-export default Badge;
-export { InfoBadge, WarnBadge };
+const InfoBadge = tw(Badge)`bg-green-200 text-green-800`;
+const MessageBadge = tw(Badge)`bg-blue-200 text-blue-800`;
+const WarnBadge = tw(Badge)`bg-red-200 text-red-800`;
+
+export default BadgeContainer;
+export { Badge, MessageBadge, InfoBadge, WarnBadge };
