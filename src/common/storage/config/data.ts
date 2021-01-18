b5ca484197ae05fcb2ea4e68d3c5291c8e029f0d
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-01-19 02:39:48 +0700

    perf(common): add debug mode on config file

--- src/common/storage/config/data.ts
+++ src/common/storage/config/data.ts
@@ -1,16 +1,20 @@
 import Value from "../core/value";
 
 interface ConfigValue extends Value {
+  debugMode: boolean;
   modDirectory: string;
   recursiveLimit: number;
   nexusmodsApiKey: string;
 }
 
 const defaults: ConfigValue = {
+  debugMode: false,
   modDirectory: "",
   recursiveLimit: 5,
   nexusmodsApiKey: "",
 };
 
+type ConfigKey = keyof ConfigValue;
+
 export default ConfigValue;
-export { defaults };
+export { defaults, ConfigKey };
