43b7d6aa90ed6168da21c87907e6abbcc859d1a0
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-01-19 02:40:25 +0700

    chore(comp): add center and left container implement with flexbox

---
+++ src/renderer/components/container/CenterContainer.tsx
@@ -0,0 +1,7 @@
+import tw from "twin.macro";
+
+const CenterContainer = tw.div`
+  flex flex-col justify-center items-center
+`;
+
+export default CenterContainer;
---
+++ src/renderer/components/container/LeftContainer.tsx
@@ -0,0 +1,7 @@
+import tw from "twin.macro";
+
+const LeftContainer = tw.div`
+  flex flex-col justify-center items-stretch
+`;
+
+export default LeftContainer;
