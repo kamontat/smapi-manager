c79df4613baa8313fa54139d2552017840e4e04c
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-01-19 02:37:58 +0700

    fix(common): json file is not correctly loaded due to json format is not correct

--- src/common/file-system/read-file.ts
+++ src/common/file-system/read-file.ts
@@ -1,6 +1,16 @@
 import { readFileSync as coreReadFileSync, readFile as readFileCB } from "fs";
 import { promisify } from "util";
 
+import jsonrepair from "jsonrepair";
+
+const parseJson = <T>(json: string): T => {
+  try {
+    return JSON.parse(json);
+  } catch (e) {
+    return JSON.parse(jsonrepair(json));
+  }
+};
+
 const readFileSync = (filepath: string): string => {
   return coreReadFileSync(filepath, { encoding: "utf-8" });
 };
@@ -8,7 +18,7 @@ const readFileSync = (filepath: string): string => {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const readJsonFileSync = async <T extends Record<string, any>>(filepath: string): Promise<T> => {
   const content = await readFileSync(filepath);
-  return JSON.parse(content) as T;
+  return parseJson<T>(content);
 };
 
 const coreReadFile = promisify(readFileCB);
@@ -20,7 +30,7 @@ const readFile = (filepath: string): Promise<string> => {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const readJsonFile = async <T extends Record<string, any>>(filepath: string): Promise<T> => {
   const content = await readFile(filepath);
-  return JSON.parse(content) as T;
+  return parseJson<T>(content);
 };
 
 export { readFile, readJsonFile, readFileSync, readJsonFileSync };
