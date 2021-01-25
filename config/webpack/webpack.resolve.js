5ec5ffe151dc9bf8ac536d299e3d1d2b9eb84c1b
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-01-25 13:36:57 +0700

    chore(config): add more alias path @states, @layouts

--- config/webpack/webpack.resolve.js
+++ config/webpack/webpack.resolve.js
@@ -3,11 +3,14 @@ const path = require("path"); // eslint-disable-line @typescript-eslint/no-var-r
 const pwd = process.cwd();
 
 module.exports = {
-  extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".mjs", ".js", ".svelte"],
+  extensions: [".mjs", ".js", ".ts", ".css", ".scss", ".sass", ".svelte", ".html"],
   alias: {
     svelte: path.resolve(pwd, "node_modules", "svelte"),
     "@common": path.resolve(pwd, "src", "common"),
     "@components": path.resolve(pwd, "src", "renderer", "components"),
     "@pages": path.resolve(pwd, "src", "renderer", "pages"),
+    "@layouts": path.resolve(pwd, "src", "renderer", "layouts"),
+    "@styles": path.resolve(pwd, "src", "renderer", "styles"),
+    "@states": path.resolve(pwd, "src", "renderer", "states")
   },
 };
