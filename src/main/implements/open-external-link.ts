97df623518b6176797bce362d5ecee4b7d2bc6ef
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-02-02 15:52:08 +0700

    perf(main): add log when user open external link

--- src/main/implements/open-external-link.ts
+++ src/main/implements/open-external-link.ts
@@ -1,8 +1,11 @@
 import { shell } from "electron";
 
 import { MainAPIs, OPEN_EXTERNAL_LINK } from "@common/communication";
+import { Logger } from "@common/logger";
 
+const logger = Logger.Common("open-external-link");
 const openExternalLink: MainAPIs[typeof OPEN_EXTERNAL_LINK] = async ({ data }) => {
+  logger.debug(`open external link (${data.input})`);
   shell.openExternal(data.input);
 };
 
