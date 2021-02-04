6d9805f049cf37716b165877c9ee17a81b6cb4d7
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-02-04 20:52:19 +0700

    chore(main): update implements styles

--- src/main/implements/open-file.ts
+++ src/main/implements/open-file.ts
@@ -1,8 +1,8 @@
 import { shell } from "electron";
-import { DataOrigin, MainAPIs, OPEN_FILE } from "@common/communication";
+import { MainAPIs, OPEN_FILE } from "@common/communication";
 import { Logger } from "@common/logger";
 
-const logger = new Logger(DataOrigin.COMMON, "open-file");
+const logger = Logger.Common("open-file");
 const openFile: MainAPIs[typeof OPEN_FILE] = async ({ data }) => {
   logger.debug(`try to open directory at ${data.input}`);
   shell.showItemInFolder(data.input);
--- src/main/implements/open-storage.ts
+++ src/main/implements/open-storage.ts
@@ -2,12 +2,12 @@ import { MainAPIs, OPEN_STORAGE } from "@common/communication";
 import { Logger } from "@common/logger";
 
 const logger = Logger.Common("open-storage");
-const openFile: MainAPIs[typeof OPEN_STORAGE] = async ({ store, data }) => {
+const openStorage: MainAPIs[typeof OPEN_STORAGE] = async ({ store, data }) => {
   const storage = store[data.subtype];
   logger.debug(`try to open directory at ${storage.url}`);
 
   storage.open();
 };
 
-export default openFile;
+export default openStorage;
 export { OPEN_STORAGE };
