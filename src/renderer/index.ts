802b8a2a68c76bee0a54f92ff4a56a18c2264a9b
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-02-10 22:45:36 +0700

    fix(ui): logging on renderer process is missing

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,9 +1,9 @@
-// import { readAppInfo } from "@common/communication";
-// import { Global } from "@common/logger";
+import { readAppInfo } from "@common/communication";
+import { Global } from "@common/logger";
 
 import App from "./App.svelte";
 
-// window.api.send(readAppInfo()).then(v => Global.byEnvironment(v.output.env));
+window.api.send(readAppInfo()).then(v => Global.byEnvironment(v.output.env));
 
 const app = new App({
   target: document.getElementById("root"),
