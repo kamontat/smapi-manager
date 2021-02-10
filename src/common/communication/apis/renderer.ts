b177bbe17e9d21f3081532ffb7c519cbb6e7cac4
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-02-11 00:03:20 +0700

    fix(common): wrong type on renderer apis return value

--- src/common/communication/apis/renderer.ts
+++ src/common/communication/apis/renderer.ts
@@ -1,9 +1,10 @@
+import type { DataCarrier } from "../models/data-carrier";
 import type { DataMapper } from "../models/data-mapper";
 
 const apiName = "api";
 
 interface RendererAPIs {
-  send: <M extends DataMapper<string>>(input: M) => Promise<M>;
+  send: <M extends DataMapper<string>>(input: M) => Promise<DataCarrier<M>>;
 }
 
 export { apiName };
