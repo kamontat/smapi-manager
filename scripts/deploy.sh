f2ef67c542ec08090c0e781d59d09fab9fe58526
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-02-11 17:58:12 +0700

    chore(script): improve deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+line="-------------------------------------------------------"
+dryrun="${1:-$DRYRUN}"
+
+is_dryrun() {
+  test -n "$dryrun"
+}
+
 is_exist() {
   git tag | grep -q "$1"
 }
@@ -8,17 +15,67 @@ cmd_exist() {
   command -v "$1" >/dev/null
 }
 
+cmd() {
+  base="$1"
+  shift
+  args=("$@")
+
+  if cmd_exist "$base"; then
+    if is_dryrun; then
+      echo "$ ${base} ${args[*]}"
+      return 0
+    fi
+
+    ${base} "${args[@]}"
+  else
+    echo "command $base is not exist (exit 3)" &&
+      return 3
+  fi
+}
+
+step_count=1
+step() {
+  local step_name="$1" exitcode=0
+  shift
+  local args=("$@")
+
+  printf "Step #%03d: %s\n\n" "${step_count}" "${step_name}"
+
+  cmd "${args[@]}"
+  exitcode=$?
+
+  if ! is_dryrun; then
+    if ((exitcode > 0)); then
+      echo "[ERROR] execute '${args[*]}' return code='$exitcode'"
+      exit "$exitcode"
+    else
+      echo
+      echo "$line"
+      echo
+    fi
+  else
+    echo
+    echo "$line"
+    echo
+  fi
+
+  ((step_count += 1))
+}
+
 pjson="$PWD/package.json"
 version="v$(jq -r ".version" "$pjson")"
 
 is_exist "$version" && echo "Please update new version in $pjson file (exit 2)" >&2 && exit 2
 ! cmd_exist "gitgo" && echo "gitgo cli is required (exit 3)" >&2 && exit 3
 
-echo "Deploying version '$version' [enter]..."
+printf "Deploying version '%s' [enter]..." "$version"
 read -r
+echo
+echo
 
-gitgo changelog --tag "$version" &&
-  git commit -am "chore(release): $version" &&
-  git tag "$version" &&
-  git push &&
-  git push --tags
+step "update changelog" gitgo changelog --tag "$version"
+step "update line-of-code file" bash "$PWD/scripts/loc.sh"
+step "git commit" git commit -am "chore(release): $version"
+step "git tag" git tag "$version"
+step "git push" git push
+step "git push tag" git push --tags
